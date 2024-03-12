043bad1f1caab1488da239bf4f474065a3588a28
mdjehidalam <146694343+mdjehidalam@users.noreply.github.com>
2024-03-12 11:09:53 +0530

    Data Types

---
+++ Day-2_Solution.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <iomanip>
+#include <limits>
+
+using namespace std;
+
+int main() {
+    int i = 4;
+    double d = 4.0;
+    string s = "HackerRank ";
+    int j;
+    double d1;
+    string s1;
+    cin>>j>>d1;
+    cin.ignore ();
+    getline(cin,s1);
+    cout<<i+j<<endl;
+    cout<<fixed<<setprecision(1)<<d+d1<<endl;
+    cout<<s+s1<<endl;
+    
+    
+    // Declare second integer, double, and String variables.
+    
+    // Read and save an integer, double, and String to your variables.
+    // Note: If you have trouble reading the entire string, please go back and review the Tutorial closely.
+    
+    // Print the sum of both integer variables on a new line.
+    
+    // Print the sum of the double variables on a new line.
+    
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+    
+    
+    return 0;
+}
